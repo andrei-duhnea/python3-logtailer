e30d976ebe62f61986dd64f22b95573040b474f6
Andrei Duhnea <andrei.duhnea@onem.com>
2016-01-21 10:31:57 +0200

    Scrips dir with ONEm all-logs tailer helper.

---
+++ scripts/localclient.py
@@ -0,0 +1,32 @@
+import time
+from socket import socket, AF_INET, SOCK_STREAM
+import sys
+import os
+
+def follow(the_file):
+    the_file.seek(0, 2)
+    while True:
+        file_line = the_file.readline()
+        if not file_line:
+            time.sleep(0.1)
+            continue
+        yield file_line
+
+
+def socket_send(msg):
+    sock = socket(AF_INET, SOCK_STREAM)
+    try:
+        sock.connect(('localhost', 20000))
+        sock.sendall(bytes(msg + "\n", 'utf-8'))
+    except ConnectionRefusedError:
+        print("Error connecting to server.")
+    finally:
+        sock.close()
+
+if __name__ == '__main__':
+    log_path = sys.argv[1]
+    log_name = os.path.basename(log_path)
+    logfile = open(log_path, 'r')
+    log_lines = follow(logfile)
+    for line in log_lines:
+        socket_send('[{}] {}'.format(log_name, line))
---
+++ scripts/localtails.sh
@@ -0,0 +1,12 @@
+LOGS_DIR=$HOME/src/logs
+LOGS=(iapi.err iapi.log sapi.err kannel-routing.log sms-routing.err)
+PY=python3
+TAILER_DIR=$HOME/src/python3-logtailer
+
+# Kill logtailer clients
+arr=($(ps aux | grep "$PY .*localclient.py" | awk '{print $2}')); for pid in ${arr[@]}; do kill $pid; done
+
+for log in ${LOGS[@]}; do
+	$PY $TAILER_DIR/scripts/localclient.py $LOGS_DIR/$log &
+done
+
